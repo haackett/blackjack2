202eb41c7e03bd47e2280bd2233a12393dfb50d7
Zack Martinsek <zack@joinroot.com>
2020-07-15 18:52:14 -0400

    First commit (not really)

---
+++ testDeck.py
@@ -0,0 +1,19 @@
+import unittest
+import random
+from temporaryMasterFile import Deck
+from temporaryMasterFile import Card
+from typing import List 
+
+class Tests(unittest.TestCase):
+    d = Deck()
+    
+    def test_deal(self):
+        self.assertIsNotNone(self.d.deal())
+
+    def test_shuffle(self):
+        shuffledDeck = Deck()
+        shuffledDeck.shuffle()
+        self.assertNotEqual(self.d.deal(), shuffledDeck.deal())
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
