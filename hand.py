f2fb8f8caef69fde0a0b5192c7504a7775f5a29c
Aidan Hackett <aodhanelectronic@gmail.com>
2020-07-26 14:41:28 -0400

    Added methods to hand.py
    Refactored hand_value and sum_values methods from game.py to hand.py

--- hand.py
+++ hand.py
@@ -1,8 +1,45 @@
+from typing import List
+from card import Card
+
+valueConversions = {
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    '10' : 10,
+    'Jack' : 10,
+    'Queen' : 10,
+    'King' : 10,
+    'Ace' : 11
+}
+
 class Hand:
-    def __init__(self):
-        self.cards = []
-        self.value = 0
+    def __init__(self, cards=[]):
+        self.cards = cards
         self.busted = False
 
     def add_card(self, card):
         self.cards.append(card)
+
+    def hand_value(self) -> int:
+        """ Returns the integer value of the hand, evaluating aces as 11 whenever optimal."""
+        values = []
+        for card in self.cards:
+            values.append(valueConversions[card.value])
+        while (values.count(11) != 0):
+            if (self.sum_values(values) > 21):
+                values[values.index(11)] = 1
+            else: 
+                break
+        return self.sum_values(values)
+
+    def sum_values(self, values: List[int]) -> int:
+        """ Helper method for hand_value. Returns the sum of all values in a List"""
+        sum = 0
+        for value in values:
+            sum = value + sum
+        return sum
