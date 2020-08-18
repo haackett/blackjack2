7245438a35d6212903393a0b7b551566afb730a2
Aidan Hackett <aodhanelectronic@gmail.com>
2020-08-18 17:33:08 -0400

    Refactored payement calculation from betting.py to game.py

--- blackjack/betting.py
+++ blackjack/betting.py
@@ -14,31 +14,10 @@ def bet(player : Player, bet: float) -> None:
         player.stack = player.stack - bet
         player.bet = bet
 
+def pay(player : Player, amount: float) -> None:
+    player.stack = player.stack + amount
+
 def clear_bet(player : Player) -> None:
     player.bet = 0
 
-def pay_blackjack(blackjacks : List[bool], players : List[Player]) -> None:
-    dealerHasBlackjack = blackjacks[0]
-    playerBlackjacks = blackjacks[1:]
-    for playerIndex, player in enumerate(playerBlackjacks):
-        if player:
-            if not dealerHasBlackjack:
-                players[playerIndex].stack = players[playerIndex].stack + players[playerIndex].bet * 2.5
-            else:
-                players[playerIndex].stack = players[playerIndex].stack + players[playerIndex].bet
-            clear_bet(players[playerIndex])
-def pay_players(playerStandings : List[List[int]], players : List[Player]) -> None:
-    for playerIndex, player in enumerate(playerStandings):
-        for hand in player:
-            if hand == 0:
-                players[playerIndex].stack = players[playerIndex].stack + players[playerIndex].bet
-            elif hand == 1:
-                players[playerIndex].stack = players[playerIndex].stack + players[playerIndex].bet * 2
-        clear_bet(players[playerIndex])
-    return        
 
-def get_stacks(players : List[Player]) -> List[float]:
-    stacks = []
-    for player in players:
-        stacks.append(player.stack)
-    return stacks
--- tests/test_betting.py
+++ tests/test_betting.py
@@ -24,6 +24,11 @@ class Tests(unittest.TestCase):
         bet(p, 5)
         self.assertEqual(p.bet, 5)
         self.assertEqual(p.stack, 95)
+
+    def test_pay(self):
+        p = Player()
+        pay(p, 100)
+        self.assertEqual(p.stack, 100)
     
     def clear_bet(self):
         p = Player()
@@ -31,35 +36,5 @@ class Tests(unittest.TestCase):
         clear_bet(p)
         self.assertEqual(p.bet, 0)
         
-    def test_pay_blackjack(self):
-        players = [Player(), Player()]
-        players[0].bet = 5
-        players[1].bet = 5
-        blackjacks = [True, True, False]
-        pay_blackjack(blackjacks, players)
-        self.assertEqual(players[0].stack, 5)
-        self.assertEqual(players[1].stack, 0)
-        blackjacks = [False, False, True]
-        pay_blackjack(blackjacks, players)
-        self.assertEqual(players[0].stack, 5)
-        self.assertEqual(players[1].stack, 12.5)
-
-    def pay_players(self):
-        players = [Player(), Player()]
-        players[0].bet, players[1].bet = 5, 5
-        playerStandings = [[1], [0]]
-        pay_players(playerStandings, players)
-        self.assertEqual(players[0].stack, 10)
-        self.assertEqual(players[1].stack, 5)
-        playerStandings = [[-1], [-1]]
-        self.assertEqual(players[0].stack, 10)
-        self.assertEqual(players[1].stack, 5)
-
-    def test_get_stacks(self):
-        players = [Player(), Player()]
-        players[0].stack = 5
-        players[1].stack = 10
-        self.assertEqual(get_stacks(players), [5, 10])
-
 if __name__ == '__main__':
     unittest.main(verbosity=2)
