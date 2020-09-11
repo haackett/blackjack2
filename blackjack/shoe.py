4e51cd0bfcadf67c2516187688c11b3efbf2f975
Aidan Hackett <aodhanelectronic@gmail.com>
2020-09-11 15:10:48 -0400

    Added shoe class

---
+++ blackjack/shoe.py
@@ -0,0 +1,26 @@
+from blackjack.card import Card
+from blackjack.deck import Deck
+import random
+
+values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+suits = ['Clubs','Diamonds','Hearts','Spades']
+
+class Shoe(Deck):
+    def __init__(self, numDecks):
+        self._cards = []
+        self.build(numDecks)
+    
+    def build(self, numDecks):
+        cards = []
+        for _ in range(numDecks):
+            cards.extend(self.shuffle([Card(value,suit) for value in values for suit in suits]))
+        self._cards = cards
+
+    def shuffle(self, cards):
+        cardsIn = cards
+        cardsOut = []
+        while (len(cardsIn) > 0):
+            randIndex = random.randint(0, len(cardsIn) - 1)
+            cardsOut.append(cardsIn[randIndex])
+            del cardsIn[randIndex]
+        return cardsOut
---
+++ tests/test_shoe.py
@@ -0,0 +1,20 @@
+import unittest
+import random
+from blackjack.shoe import Shoe
+from typing import List 
+
+class Tests(unittest.TestCase):
+    
+    def test_init(self):
+        s = Shoe(1)
+        self.assertEqual(len(s._cards), 52)
+
+    def test_deal(self):
+        s = Shoe(1)
+        self.assertIsNotNone(s.deal())
+
+    #TODO : a test to make sure the decks have shuffled invidiually
+    # Maybe test the distance between cards of the same value and suit?
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
